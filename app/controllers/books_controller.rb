866b76684d719ca6d92b6cb4622c6ef60ef87c7b
ahegaowpeace <arduino0202@gmail.com>
2020-08-08 20:50:16 +0900

    Add Show.html.erb

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,4 +2,8 @@ class BooksController < ApplicationController
   def index
     @books = Book.order(id: :desc).page(params[:page]).per(28)
   end
+
+	def show
+		@book = Book.find(params[:id])
+	end
 end
