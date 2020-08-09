911db50c7366379363a90aec7c1057862b6da48f
ahegaowpeace <arduino0202@gmail.com>
2020-08-10 02:51:09 +0900

    Category/Tag Controller DRY

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -14,42 +14,16 @@ class BooksController < ApplicationController
 		# カテゴリPOST処理
 		if params[:sendctg]
 			@category = Category.find_by(name: params.require(:ctgname))
-			# 既存のカテゴリの場合
-			if @category
-				@ctgrelation = Ctgrelation.new(book_id: @book.id, category_id: @category.id)
-				if @ctgrelation.save
-					redirect_to book_path(@book.id)
-				end
-			# 新規カテゴリの場合
-			else
-				@category = Category.new(name: params.require(:ctgname))
-				if @category.save
-					@ctgrelation = Ctgrelation.new(book_id: @book.id, category_id: @category.id)
-					if @ctgrelation.save
-						redirect_to book_path(@book.id)
-					end
-				end
-			end
+			@category = Category.create(name: params.require(:ctgname)) if !@category		##新規カテゴリの場合##
+			@ctgrelation = Ctgrelation.new(book_id: @book.id, category_id: @category.id)
+			redirect_to book_path(@book.id) if @ctgrelation.save
 		end
 		# タグPOST処理
 		if params[:sendtag]
 			@tag = Tag.find_by(name: params.require(:tagname))
-			# 既存のタグの場合
-			if @tag
-				@tagrelation = Tagrelation.new(book_id: @book.id, tag_id: @tag.id)
-				if @tagrelation.save
-					redirect_to book_path(@book.id)
-				end
-			# 新規タグの場合
-			else
-				@tag = Tag.new(name: params.require(:tagname))
-				if @tag.save
-					@tagrelation = Tagrelation.new(book_id: @book.id, tag_id: @tag.id)
-					if @tagrelation.save
-						redirect_to book_path(@book.id)
-					end
-				end
-			end
+			@tag = Tag.create(name: params.require(:tagname)) if !@tag		##新規タグの場合##
+			@tagrelation = Tagrelation.new(book_id: @book.id, tag_id: @tag.id)
+			redirect_to book_path(@book.id) if @tagrelation.save
 		end
 	end
 
@@ -59,16 +33,12 @@ class BooksController < ApplicationController
 			bookid = params[:id]
 			@category = Category.find_by(name: params.require(:ctgname))
 			@ctgrelation = Ctgrelation.find_by(book_id: bookid, category_id: @category.id)
-			if @ctgrelation.destroy
-				redirect_to book_path(bookid)
-			end
+			redirect_to book_path(bookid) if @ctgrelation.destroy
 		elsif params[:tagname]
 			bookid = params[:id]
 			@tag = Tag.find_by(name: params.require(:tagname))
 			@tagrelation = Tagrelation.find_by(book_id: bookid, tag_id: @tag.id)
-			if @tagrelation.destroy
-				redirect_to book_path(bookid)
-			end
+			redirect_to book_path(bookid) if @tagrelation.destroy
 		end
 	end
 end
