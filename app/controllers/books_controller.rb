98f5fd4bf46fb0e15d9a3f942b191dac0d99a9a2
ahegaowpeace <arduino0202@gmail.com>
2020-08-09 19:53:27 +0900

    Add Adding Category Function

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,5 +5,31 @@ class BooksController < ApplicationController
 
 	def show
 		@book = Book.find(params[:id])
+		@category = Category.new
+	end
+
+	def addctg
+		@book = Book.find(params[:id])
+
+		# カテゴリPOST処理
+		if params.require(:ctgname)
+			@category = Category.find_by(name: params.require(:ctgname))
+			# 既存のカテゴリの場合
+			if @category
+				@ctgrelation = Ctgrelation.new(book_id: @book.id, category_id: @category.id)
+				if @ctgrelation.save
+					redirect_to book_path(@book.id)
+				end
+			# 新規カテゴリの場合
+			else
+				@category = Category.new(name: params.require(:ctgname))
+				if @category.save
+					@ctgrelation = Ctgrelation.new(book_id: @book.id, category_id: @category.id)
+					if @ctgrelation.save
+						redirect_to book_path(@book.id)
+					end
+				end
+			end
+		end
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'books#index'
 	resources :books, only: [:index, :show]
+	post '/books/:id', to: 'books#addctg'
+
 	get '/categories', to: 'categories#index'
 	get '/categories/:category', to: 'categories#show'
 end
