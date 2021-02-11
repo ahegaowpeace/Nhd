b6f92b181110af5d057b67af982c30f05e16693b
root <root@localhost.localdomain>
2021-02-11 18:08:00 +0900

    シンボリックリンクから画像を参照出来なくなったため、public配下へ移動

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-Book.create(title: "inthefirstperson", pagenum: 14)
-Book.create(title: "このままのそれから", pagenum: 20)
-Book.create(title: "シマウマナルコレプシー", pagenum: 28)
-Book.create(title: "ジバちゃんのナツ", pagenum: 22)
-Book.create(title: "ダブルスロウチョコレート", pagenum: 22)
-Book.create(title: "トラウマセックスクリニック", pagenum: 22)
-Book.create(title: "ハーレムレーン", pagenum: 321)
-Book.create(title: "ハイスコアトリップ", pagenum: 18)
-Book.create(title: "湯る泉", pagenum: 18)
-Book.create(title: "明日もきっと想うひと", pagenum: 221)
+#Book.create(title: "inthefirstperson", pagenum: 14)
+#Book.create(title: "このままのそれから", pagenum: 20)
+#Book.create(title: "シマウマナルコレプシー", pagenum: 28)
+#Book.create(title: "ジバちゃんのナツ", pagenum: 22)
+#Book.create(title: "ダブルスロウチョコレート", pagenum: 22)
+#Book.create(title: "トラウマセックスクリニック", pagenum: 22)
+#Book.create(title: "ハーレムレーン", pagenum: 321)
